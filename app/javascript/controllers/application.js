8565a81417cc9738a1ba53e0b8df02a7a1eadf18
namae-no-nai <guilhermebluiz@hotmail.com>
2025-03-03 19:37:06 -0300

    redo

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import NestedFormController from "./controllers/nested_form_controller"
-
 
 const application = Application.start()
-application.register("nested-form", NestedFormController)
 
 // Configure Stimulus development experience
 application.debug = false
