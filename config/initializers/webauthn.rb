1d64105b3d19355f665d264e99b8f0432f772bbf
namae-no-nai <guilhermebluiz@hotmail.com>
2025-03-19 19:45:48 -0300

    init config for webauthn

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,4 +1,5 @@
 WebAuthn.configure do |config|
+  config.origin = ENV.fetch("WEBAUTHN_ORIGIN", "http://localhost:3000")
   config.allowed_origins = [ Rails.configuration.webauthn_origin ]
 
   config.rp_name = "Clube de tiro Interarmas"
